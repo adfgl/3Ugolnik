940e9c2abb54320bbfee6c1510cc50338e607f55
Pavel Zhukov <pavel.zhukov@peikko.com>
2025-06-12 11:28:16 +0300

    add Face build test

--- CDTriangulation/CDTTests/FaceSplitTests.cs
+++ CDTriangulation/CDTTests/FaceSplitTests.cs
@@ -12,11 +12,22 @@ namespace CDTTests
                 new Node(1, +30, -30), 
                 new Node(2, 0, 30));
 
-
             SplitResult result = face.Split(new Node(3, 0, 0));
 
             Assert.Equal(3, result.NewFaces.Length);
             Assert.Single(result.OldFaces);
+
+            for (int i = 0; i < 3; i++)
+            {
+                Face curr = result.NewFaces[i];
+                Face next = result.NewFaces[(i + 1) % 3];
+
+                Assert.Null(curr.Edge.Twin); 
+                Assert.False(curr.Edge.Constrained);
+
+                Assert.Same(curr.Edge.Next.Twin, next.Edge.Prev);
+                Assert.Same(next.Edge.Prev.Twin, curr.Edge.Next);
+            }
 
         }
     }
