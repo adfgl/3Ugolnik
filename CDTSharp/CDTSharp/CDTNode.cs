3cec096e47cbe2cbf1e4bced4eb6c78c6bc267b4
Pavel Zhukov <pavel.zhukov.engineer@gmail.com>
2025-06-24 11:33:04 +0300

    add CDT structures

---
+++ CDTSharp/CDTSharp/CDTNode.cs
@@ -0,0 +1,26 @@
+﻿namespace CDTSharp
+{
+    public class CDTNode
+    {
+        public CDTNode()
+        {
+            
+        }
+
+        public CDTNode(double x, double y, double z = 0)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Z { get; set; }
+
+        public override string ToString()
+        {
+            return $"{X} {Y} {Z}";
+        }
+    }
+}
